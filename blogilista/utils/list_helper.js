eba75a3f3a612e9a68670905d7a47043ea1a91b8
MikkoKarvonen <mkarvonen34@gmail.com>
2019-05-13 20:36:14 +0300

    Lisätty tehtävä 4.5

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((a, b) => a + (b.likes || 0), 0);
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.sort((a, b) => b.likes - a.likes)[0];
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
