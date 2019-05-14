af42cecec1060bf447c635b594d05f91ec250c7d
MikkoKarvonen <mkarvonen34@gmail.com>
2019-05-14 18:11:42 +0300

    Lisätty tehtävä 4.7

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -14,7 +14,6 @@ const mostEntries = (blogs) => {
     let numOfBlogs = [];
     blogs.forEach(function(blog) {
         if (numOfBlogs.some(e => e.author === blog.author)) {
-            /* vendors contains the element we're looking for */
             let found = numOfBlogs.find(e => e.author === blog.author)
             found['blogs'] = found['blogs'] + 1
         } else {
@@ -25,9 +24,24 @@ const mostEntries = (blogs) => {
     return most;
 }
 
+const mostLikes = (blogs) => {
+    let numOfLikes = [];
+    blogs.forEach(function(blog) {
+        if (numOfLikes.some(e => e.author === blog.author)) {
+            let found = numOfLikes.find(e => e.author === blog.author)
+            found['likes'] = found['likes'] + blog.likes
+        } else {
+            numOfLikes.push({author: blog.author,  likes: blog.likes})
+        }
+    });
+    let most = numOfLikes.sort((a,b)=>b.likes-a.likes)[0];   
+    return most;
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostEntries
+    mostEntries,
+    mostLikes
 }
