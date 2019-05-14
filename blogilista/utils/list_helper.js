ba5384871ca21355424aaa83aaf69672d9a6e44e
MikkoKarvonen <mkarvonen34@gmail.com>
2019-05-14 18:06:41 +0300

    Lisätty tehtävä 4.6

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -10,8 +10,24 @@ const favoriteBlog = (blogs) => {
     return blogs.sort((a, b) => b.likes - a.likes)[0];
 }
 
+const mostEntries = (blogs) => {
+    let numOfBlogs = [];
+    blogs.forEach(function(blog) {
+        if (numOfBlogs.some(e => e.author === blog.author)) {
+            /* vendors contains the element we're looking for */
+            let found = numOfBlogs.find(e => e.author === blog.author)
+            found['blogs'] = found['blogs'] + 1
+        } else {
+            numOfBlogs.push({author: blog.author,  blogs: 1})
+        }
+    });
+    let most = numOfBlogs.sort((a,b)=>b.blogs-a.blogs)[0];   
+    return most;
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostEntries
 }
